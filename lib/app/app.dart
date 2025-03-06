c8cda12efa0954bf1c7217a54657e1852875c225
Prachi <pjtp1003@gmail.com>
2025-03-06 10:30:27 +0530

    updated 1.0.5 (6) demo

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -191,7 +191,6 @@ class _App extends State<App> {
                 ),
               );
             }
-
           case '/onBordingWelcome':
             // widget.signedIn?(){}:setToken();
             return MaterialPageRoute(
