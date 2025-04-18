de89aa3f25f9b4f36f4483e793b63d096c8e25e9
shubham <shubham.wani@binyuga.com>
2025-04-18 18:21:17 +0530

    Contact dialer tab

---
+++ lib/presentation/screens/scheduler_model/sm_Intake/widgets/contactTab/calls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactCallsScreen extends StatelessWidget {
+  const ContactCallsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Calls'),
+    );
+  }
+}
---
+++ lib/presentation/screens/scheduler_model/sm_Intake/widgets/contactTab/e_fax_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContactEFaxScreen extends StatelessWidget {
+  const ContactEFaxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('E-Fax'),
+    );
+  }
+}
