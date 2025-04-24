5091a666233c6f38b1ce2e4ccbdc42a96aef0fdb
prajwalbin <prajwallok8@gmail.com>
2025-04-24 18:11:31 +0530

    working on sm  in that fix bug and alignment in call merge sp fix bug .. merge sw chatbox alignment

--- lib/presentation/screens/scheduler_model/sm_scheduler/widget/schedular/widget/pending_page.dart
+++ lib/presentation/screens/scheduler_model/sm_scheduler/widget/schedular/widget/pending_page.dart
@@ -685,8 +685,8 @@ class _PendingPageViewState extends State<PendingPageView> {
                 color: Color(0xFFF7F8FA),
                 borderRadius: BorderRadius.circular(10),
               ),
-          height: 450,
-          width: 500,
+          height: 460,
+          width: 550,
 
 
               child: ChatBotContainer(
