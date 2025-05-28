1888f6f4fac33ee8036d9b79dc07b32c0f883fa4
ShubhDev <shubham.dev@symmetry.care>
2025-05-28 11:50:19 +0530

    Prod search zip code funtionality

--- lib/presentation/screens/em_module/company_identity/widgets/company_identity_zone/widgets/location_screen.dart
+++ lib/presentation/screens/em_module/company_identity/widgets/company_identity_zone/widgets/location_screen.dart
@@ -1,127 +1,180 @@
+import 'dart:convert';
+import 'dart:html' as html; // For browser back button
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:prohealth/app/resources/color.dart';
-import 'package:prohealth/presentation/screens/em_module/widgets/button_constant.dart';
-import '../../../../../../../app/resources/establishment_resources/establish_theme_manager.dart';
-import '../../../../../../../app/resources/value_manager.dart';
+import 'package:http/http.dart' as http;
+import 'package:prohealth/app/constants/app_config.dart'; // Replace with your actual config path
 
 class MapScreen extends StatefulWidget {
   final LatLng initialLocation;
   final Function(LatLng) onLocationPicked;
 
-  MapScreen({required this.initialLocation, required this.onLocationPicked});
+  const MapScreen({
+    Key? key,
+    required this.initialLocation,
+    required this.onLocationPicked,
+  }) : super(key: key);
 
   @override
-  _MapScreenState createState() => _MapScreenState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController _mapController;
   late LatLng _selectedLocation;
+  final TextEditingController _searchController = TextEditingController();
+
+  final String _googleApiKey = AppConfig.googleApiKey;
 
   @override
   void initState() {
     super.initState();
     _selectedLocation = widget.initialLocation;
+
+    // 👇 Handle browser back button (Flutter Web only)
+    html.window.onPopState.listen((event) {
+      if (Navigator.canPop(context)) {
+        Navigator.of(context).pop();
+      }
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
   }
 
-  void _onTap(LatLng location) {
+  void _onTap(LatLng position) {
     setState(() {
-      _selectedLocation = location;
+      _selectedLocation = position;
     });
   }
 
-  ///old
   void _confirmSelection() {
     widget.onLocationPicked(_selectedLocation);
-    print("Picked Location: $_selectedLocation");
-    Navigator.of(context).pop(_selectedLocation);
+    Navigator.pop(context, _selectedLocation);
   }
-  ///updated
-  // void _confirmSelection() {
-  //   if (!mounted) return; // Ensure the widget is still active
-  //
-  //   if (_selectedLocation != null) {
-  //     widget.onLocationPicked(_selectedLocation!);
-  //     debugPrint("Picked Location: $_selectedLocation");
-  //
-  //     if (Navigator.canPop(context)) {
-  //       Navigator.of(context).pop(_selectedLocation);
-  //     }
-  //   } else {
-  //     debugPrint("No location selected");
-  //   }
-  // }
+
+  Future<void> _searchZip(String zipCode) async {
+    try {
+      final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?address=$zipCode&key=$_googleApiKey',
+      );
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        if (data['status'] == 'OK') {
+          final location = data['results'][0]['geometry']['location'];
+          final latLng = LatLng(location['lat'], location['lng']);
+
+          _mapController.animateCamera(
+            CameraUpdate.newLatLngZoom(latLng, 14.0),
+          );
+
+          setState(() {
+            _selectedLocation = latLng;
+            print('Search lat long ${_selectedLocation}');
+          });
+        } else {
+          _showSnackBar("No results found for ZIP code.");
+        }
+      } else {
+        _showSnackBar("API error: ${response.statusCode}");
+      }
+    } catch (e) {
+      _showSnackBar("Failed to fetch location: $e");
+    }
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(
-            child: GoogleMap(
-              onMapCreated: _onMapCreated,
-              onTap: _onTap,
-              initialCameraPosition: CameraPosition(
-                target: _selectedLocation,
-                zoom: 14.0,
-              ),
-              markers: {
-                Marker(
-                  markerId: MarkerId('selectedLocation'),
-                  position: _selectedLocation,
-                ),
-              },
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            onTap: _onTap,
+            initialCameraPosition: CameraPosition(
+              target: _selectedLocation,
+              zoom: 14,
             ),
+            markers: {
+              Marker(
+                markerId: MarkerId('selected'),
+                position: _selectedLocation,
+              )
+            },
           ),
           Positioned(
             top: 40,
             right: 16,
-            child: CustomElevatedButton(
-            width: AppSize.s120,
-            height: AppSize.s32,
-              text: "Confirm",
-              color: ColorManager.blueprime,
-              onPressed: _confirmSelection,
-            )
+            left: 16,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 320,
+                  height: 37,
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    controller: _searchController,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (val) {
+                      final zip = val.trim();
+                      if (zip.isNotEmpty) {
+                        _searchZip(zip);
+                      }
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Search ZIP Code",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical:7),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () {
+                          final zip = _searchController.text.trim();
+                          if (zip.isNotEmpty) {
+                            _searchZip(zip);
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _confirmSelection,
+                  child: Text("Confirm"),
+                ),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       backgroundColor: ColorManager.blueprime,
-  //       title: Text('Pick Location',style: TableHeading.customTextStyle(context)),
-  //       actions: [
-  //         IconButton(
-  //           icon: Icon(Icons.check,color: ColorManager.white),
-  //           onPressed: _confirmSelection,
-  //         ),
-  //       ],
-  //     ),
-  //     body: GoogleMap(
-  //       onMapCreated: _onMapCreated,
-  //       onTap: _onTap,
-  //       initialCameraPosition: CameraPosition(
-  //         target: _selectedLocation,
-  //         zoom: 14.0,
-  //       ),
-  //       markers: {
-  //         Marker(
-  //           markerId: MarkerId('selectedLocation'),
-  //           position: _selectedLocation,
-  //         ),
-  //       },
-  //     ),
-  //   );
-  // }
 }
